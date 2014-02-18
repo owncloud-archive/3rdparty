624022423304280dab2cf7ed2367aa7c99ff565c
Bart Visscher <bartv@thisnet.nl>
2014-02-18 18:06:30 +0100

    doctrine/dbal#516 PR

--- doctrine/dbal/tests/Doctrine/Tests/DBAL/Functional/Schema/SqliteSchemaManagerTest.php
+++ doctrine/dbal/tests/Doctrine/Tests/DBAL/Functional/Schema/SqliteSchemaManagerTest.php
@@ -43,4 +43,27 @@ class SqliteSchemaManagerTest extends SchemaManagerFunctionalTestCase
           . 'auto-increment. So, while it does support a single identity column, we cannot with '
           . 'certainty determine which it is.');
     }
+
+    public function testNonDefaultPKOrder()
+    {
+        $version = \SQLite3::version();
+        if(version_compare($version['versionString'], '3.7.16', '<')) {
+            $this->markTestSkipped('This version of sqlite doesn\'t return the order of the Primary Key.');
+        }
+        $this->_conn->executeQuery(<<<EOS
+CREATE TABLE non_default_pk_order (
+    id INTEGER,
+    other_id INTEGER,
+    PRIMARY KEY(other_id, id)
+)
+EOS
+        );
+
+        $tableIndexes = $this->_sm->listTableIndexes('non_default_pk_order');
+
+         $this->assertEquals(1, count($tableIndexes));
+
+        $this->assertArrayHasKey('primary', $tableIndexes, 'listTableIndexes() has to return a "primary" array key.');
+        $this->assertEquals(array('other_id', 'id'), array_map('strtolower', $tableIndexes['primary']->getColumns()));
+    }
 }
